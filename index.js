61ddb072d0bb24b38c3ff0a3b5d29368f47b1386
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-13 19:03:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
-  if (left < 360-parseInt(dodger.width)) {
+  if (left < 10000) {
     dodger.style.left = `${left + 1}px`;
   }
 }
